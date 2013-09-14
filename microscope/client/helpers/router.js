288dba7062c095b3844e10f4189ff3439c630752
andrewmelis <amelis@pathf.com>
2013-09-14 13:42:41 -0500

    Finished 5-2, routing to a single post page

--- microscope/client/helpers/router.js
+++ microscope/client/helpers/router.js
@@ -1,3 +1,7 @@
 Meteor.Router.add({
-  '/': 'postsList'
+  '/': 'postsList',
+  '/posts/:_id': {
+    to: 'postPage',   //maps to template name, in this case, postPage (doubles as name of our route)
+    and: function(id) { Session.set('currentPostId', id); }	//auxiliary function sets Session variable 'currentPostId' to the id pulled out of url
+  }
 });
