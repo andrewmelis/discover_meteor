48c28d7d7c8c3d61274ba63c8859d1db8adb0c80
andrewmelis <amelis@pathf.com>
2013-09-21 21:40:45 -0500

    finished 7-2. removed insecure package, and allowed certain writes to posts

--- microscope/collections/posts.js
+++ microscope/collections/posts.js
@@ -1,2 +1,8 @@
 Posts = new Meteor.Collection('posts');	//omitting var bc want global scope for Posts
+Posts.allow({
+  insert: function(userId, doc) {
+    //only allow posting if you are logged in
+    return !! userId;
+  }
+});
 
